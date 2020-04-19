fd1b387cd303f5768399cd3090dade414f696068
dmi3coder <dmi3coder@gmail.com>
2020-04-19 16:46:47 +0300

    Using url instead of string to read python file

--- src/main/java/tech/donau/quarkify/EffectResource.java
+++ src/main/java/tech/donau/quarkify/EffectResource.java
@@ -1,6 +1,7 @@
 package tech.donau.quarkify;
 
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 
 import javax.ws.rs.GET;
@@ -8,9 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.net.URL;
 
 @Path("/effect")
 public class EffectResource {
@@ -18,12 +17,10 @@ public class EffectResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() throws IOException {
-        String effectPy = new String(Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("effect.py").getPath())), StandardCharsets.UTF_8);
+        final URL effectPy = getClass().getClassLoader().getResource("effect.py");
         System.out.println(effectPy);
         Context context = Context.newBuilder().allowIO(true).allowAllAccess(true).build();
-	System.out.println("before");
-        Value effectValue = context.eval("python", effectPy);
-	System.out.println("aftter");
+        Value effectValue = context.eval(Source.newBuilder("python", effectPy).build());
         return effectValue.toString();
     }
 }
