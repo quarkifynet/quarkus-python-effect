8e9d90c323b09b7b26b69936e513282269720253
Dmytro Chaban <dmi3coder@gmail.com>
2020-04-19 12:26:17 +0000

    Initial commit

---
+++ src/test/java/tech/donau/quarkify/NativeEffectResourceIT.java
@@ -0,0 +1,9 @@
+package tech.donau.quarkify;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeEffectResourceIT extends EffectResourceTest {
+
+    // Execute the same tests but in native mode.
+}
